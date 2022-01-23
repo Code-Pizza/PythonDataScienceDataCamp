212d6bd05e112f98bce6d29f6ba2317d8c9bcdce
Code-Pizza <brian@monedero.io>
2022-01-23 14:01:15 -0800

    Completed Intro to Seaborn Chapter 1

---
+++ Introduction to MatPlotLib/rotation_period = {.py
@@ -0,0 +1,8 @@
+datacamp = {
+    "course": "python",
+    "level": "intermediate",
+    "lesson": {"dictionaries": "python", "lists": "r"},
+}
+datacamp["lesson"]["lists"] = "sql"
+print(datacamp)
+# {'course': 'python', 'level': 'intermediate', 'lesson': {'dictionaries': 'python', 'lists': 'sql'}}
